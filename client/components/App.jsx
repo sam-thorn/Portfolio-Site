aa5ccb1d7c4c4f9cd234c8d42022ae6fdf32e8de
Sam Thorn <spthornnz@gmail.com>
2021-05-28 15:45:02 +1200

    Sets up file structure and base components

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// import Nav from './Nav'
+// import Home from './Home'
+
+function App () {
+  return (
+    <>
+      <div>
+        <Nav />
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
