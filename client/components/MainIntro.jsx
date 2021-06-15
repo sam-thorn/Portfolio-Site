f61d00224cc5f55401dabba583a5c1ecf8ff83e2
Sam Thorn <spthornnz@gmail.com>
2021-06-15 18:03:01 +1200

    Basic main page layout established

--- client/components/MainIntro.jsx
+++ client/components/MainIntro.jsx
@@ -5,10 +5,10 @@ function MainIntro () {
         <>
             <div className='intro-container'>
                 <div className='intro-text-container'>
-                    <h1>Hello</h1>
+                    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut voluptates obcaecati minus expedita? Labore cum voluptates quaerat autem laborum quasi modi! Dolore expedita assumenda consequuntur nisi commodi eum laborum velit?</h1>
                 </div>     
                 <div className='intro-image-container'>
-                    <img className='hero-image' src='images/hero-image-bw.jpg' alt='a picture of Sam'/>
+                <img src='images/face-copy.png' alt='a cartoon face' style={{height: 270 + 'px'}}/>
                 </div>
             </div>
         </>
