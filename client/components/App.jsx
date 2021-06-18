2e6c684bb3ed9a400a148de9a22aff138b7e17a1
Sam Thorn <spthornnz@gmail.com>
2021-06-18 16:42:43 +1200

    Minor adjustments in readiness for react router connect

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
